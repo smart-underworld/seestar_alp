7a68c52b5da5adcfd2ce9b586a70ba0e25430e4c
Ben Guthro <ben@chaossearch.io>
2024-09-26 16:55:02 -0400

    Add version.py

---
+++ device/version.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import subprocess
+
+if getattr(sys, "frozen",  False):
+    search_path = sys._MEIPASS
+else:
+    search_path = os.path.join(os.path.dirname(__file__))
+
+class Version:
+    # https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
+    # Return the git revision as a string
+    @staticmethod
+    def git_version():
+        def _minimal_ext_cmd(cmd):
+            # construct minimal environment
+            env = {}
+            for k in ['SYSTEMROOT', 'PATH']:
+                v = os.environ.get(k)
+                if v is not None:
+                    env[k] = v
+            # LANGUAGE is used on win32
+            env['LANGUAGE'] = 'C'
+            env['LANG'] = 'C'
+            env['LC_ALL'] = 'C'
+            out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+            return out
+
+        try:
+            out = _minimal_ext_cmd(['git', 'describe', '--tags'])
+            GIT_REVISION = out.strip().decode('ascii')
+        except OSError:
+            GIT_REVISION = "Unknown"
+
+        return GIT_REVISION
+
+    @staticmethod
+    def app_version():
+        path_to_ver = os.path.abspath(os.path.join(search_path, "version.txt"))
+        if not os.path.exists(path_to_ver):
+            return Version.git_version()
+        else:
+            with open(path_to_ver, 'r') as file:
+                return file.read().replace('\n', '')
+
+
+
+if __name__ == "__main__":
+    print(f"VER: {Version.app_version()}")
