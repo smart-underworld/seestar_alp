71947753fbed8051072e85517f277bbd5f652545
Brian <bhauber@gmail.com>
2024-09-28 15:12:45 -0500

    Disabling 'stack_masic' on Settings page

--- front/app.py
+++ front/app.py
@@ -489,7 +489,7 @@ def get_device_settings(telescope_id):
         "light_duration_min": stack_settings_result["light_duration_min"],
         "auto_3ppa_calib": settings_result["auto_3ppa_calib"],
         "frame_calib": settings_result["frame_calib"],
-        "stack_masic": settings_result["stack_masic"],
+        #"stack_masic": settings_result["stack_masic"],
         # "rec_stablzn": settings_result["rec_stablzn"], # Unavailable for firmware 3.11
         "manual_exp": settings_result["manual_exp"],
         # "isp_exp_ms": settings_result["isp_exp_ms"],
@@ -1593,7 +1593,7 @@ class SettingsResource:
             "auto_power_off": str2bool(PostedSettings["auto_power_off"]),
             "auto_3ppa_calib": str2bool(PostedSettings["auto_3ppa_calib"]),
             "frame_calib": str2bool(PostedSettings["frame_calib"]),
-            "stack_masic": str2bool(PostedSettings["stack_masic"]),
+            #"stack_masic": str2bool(PostedSettings["stack_masic"]),
             # "rec_stablzn": str2bool(PostedSettings["rec_stablzn"]),
             "manual_exp": str2bool(PostedSettings["manual_exp"])
         }
