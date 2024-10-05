1a543f0ecbe21367963678190220ab15c8013af6
Steven Byrnes <erewhon@flatland.org>
2024-10-05 16:19:04 -0500

    Fix live view modes
    The following modes were missing, broken, or not behaving as expected:
    - sun
    - moon
    - planet
    
    Additional, fixed a bug in Live View mode if you haven't specified a
    mode previously.  (Mode was null.)

---
+++ lib/__init__.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from time import time
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f'Function {f.__name__} took {te - ts:2.4f} seconds')
+        #print('func:%r args:[%r, %r] took: %2.4f sec' % \
+        #  (f.__name__, args, kw, te-ts))
+        return result
+    return wrap
